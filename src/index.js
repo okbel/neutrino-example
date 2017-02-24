63574a39669d19c6c82c857896e83b853a679d67
Belen Curcio <curciobelen@gmail.com>
2017-02-24 09:18:38 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+import App from './App'
+
+const load = () => render((
+  <AppContainer>
+    <App />
+  </AppContainer>
+), document.getElementById('root'))
+
+// Hot Module Loader
+
+if (module.hot) {
+  module.hot.accept('./App', load)
+}
+
+load()
